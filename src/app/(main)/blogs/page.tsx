e30ddf8f53f0c070c290d350bec0bbf3c03b9dca
GauravSinghdev <gauravkalakoti19@gmail.com>
2025-08-15 16:03:15 +0530

    fixing css - v1

--- src/app/(main)/blogs/page.tsx
+++ src/app/(main)/blogs/page.tsx
@@ -6,9 +6,9 @@ import InfiniteBlogScroll from "./InfiniteBlogScroll";
 export default function AllBlogPage() {
   return (
     <main className="max-w-5xl mx-auto flex flex-col gap-5 min-h-screen">
-      <div className="flex justify-end mt-20">
+      <div className="flex justify-start mt-24">
         <Link href={"/create-blog"}>
-          <Button className="w-fit mt-2 mr-2 md:mr-0 font-semibold">
+          <Button className="w-fit ml-2 md:mr-0 font-semibold">
             Create new Blog
           </Button>
         </Link>
