39956c9368d6edf60215bec82cd99925ef6ec13e
GauravSinghdev <gauravkalakoti19@gmail.com>
2025-08-19 02:14:20 +0530

    adding logo and fixing appbar

--- src/components/shared/Appbar.tsx
+++ src/components/shared/Appbar.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { BookOpen } from "lucide-react";
+import logo from "../../../public/logo.png"
 import { motion } from "framer-motion";
 import Link from "next/link";
 import { UserBtn } from "./UserBtn";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const navArr = [
   {
@@ -17,6 +19,8 @@ const navArr = [
 ];
 
 export default function Appbar() {
+  const pathname = usePathname();
+
   return (
     <motion.nav
       initial={{ opacity: 0, y: -20 }}
@@ -27,7 +31,7 @@ export default function Appbar() {
       <div className=" bg-white/60 dark:bg-slate-900/30 border-2 border-white/20 dark:border-slate-700/40 rounded-xl px-6 py-3 shadow flex justify-between items-center gap-5">
         {/* Logo */}
         <Link href="/" className="flex items-center gap-2">
-          <BookOpen className="h-8 w-8 text-blue-600 dark:text-blue-400" />
+          <Image src={logo} alt="logo" width={20} height={20} className="h-8 w-8" priority/>
           <span className="hidden md:block text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 dark:from-blue-400 dark:to-purple-400 bg-clip-text text-transparent">
             ByteLog
           </span>
@@ -35,21 +39,26 @@ export default function Appbar() {
 
         {/* Desktop Menu */}
         <div className="flex items-center gap-4 sm:gap-8">
-          {navArr.map((item) => (
-            <Link
-              key={item.name}
-              href={item.link}
-              className="text-slate-600 dark:text-slate-300 hover:text-slate-900 dark:hover:text-white transition-colors"
-            >
-              {item.name}
-            </Link>
-          ))}
+          {navArr.map((item) => {
+            const isActive = pathname.startsWith(item.link);
+            return (
+              <Link
+                key={item.name}
+                href={item.link}
+                className={`transition-colors font-semibold ${
+                  isActive
+                    ? "text-blue-600 dark:text-blue-400 underline underline-offset-4"
+                    : "text-slate-600 dark:text-slate-300 hover:text-slate-900 dark:hover:text-white"
+                }`}
+              >
+                {item.name}
+              </Link>
+            );
+          })}
         </div>
 
-          {" "}
-          <UserBtn />
-
-        </div>
+        <UserBtn />
+      </div>
     </motion.nav>
   );
 }
