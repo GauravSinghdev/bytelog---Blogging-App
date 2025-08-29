4f7b21f5ffb2f7a7be9d950a8c9aa8e368449680
GauravSinghdev <gauravkalakoti19@gmail.com>
2025-08-29 07:51:09 +0530

    adding sitemap.ts file

---
+++ src/app/sitemap.ts
@@ -0,0 +1,10 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://bytelog.codewithkara.com",
+      lastModified: "2025-06-29",
+    },
+  ];
+}
