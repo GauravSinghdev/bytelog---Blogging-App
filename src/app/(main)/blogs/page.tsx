329e352da15520d33367856a6d19cd3e51b2a08c
GauravSinghdev <gauravkalakoti19@gmail.com>
2025-09-17 11:53:01 +0530

    fixing bug of pagination - v2

--- src/app/(main)/blogs/page.tsx
+++ src/app/(main)/blogs/page.tsx
@@ -1,10 +1,13 @@
-import React from "react";
+// page.tsx or page.js under /blogs
+import React, { Suspense } from "react";
 import AllBlogComp from "./AllBlogComp";
 
 export default function AllBlogPage() {
   return (
     <main className="max-w-6xl mx-auto flex flex-col gap-5 min-h-screen">
-      <AllBlogComp/>
+      <Suspense fallback={<div>Loading blogs...</div>}>
+        <AllBlogComp />
+      </Suspense>
     </main>
   );
 }
